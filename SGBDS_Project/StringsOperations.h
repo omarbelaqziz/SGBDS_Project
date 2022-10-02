4c93ec48e8dfa460870bc9905f7c68c6efd8a21d
mo-hunter123 <ahrrass.00@gmail.com>
2022-10-02 16:47:15 +0100

    Making strings manipulation as class

---
+++ SGBDS_Project/StringsOperations.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class StringsOperations
+{
+
+public:
+    static vector<string> split(const string &str);
+    static string rtrim(const string &str);
+    static string ltrim(const string &str);
+}; 
