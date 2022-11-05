fc522ff610ade775dfc6a127eeec39dad9c4bc71
mo-hunter123 <ahrrass.00@gmail.com>
2022-11-05 15:20:47 +0000

    adding separation by shell scripts

---
+++ SGBDS_Project/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+if [[ $# -eq 0 ]] ; then 
+    echo "Please specify the correct livrable to compile"
+    exit 1
+fi
+
+# CCOPT="-m64 -O -fPIC -fno-strict-aliasing -fexceptions -DNDEBUG -DIL_STD"
+# CCFLAGS=$CCOPT
+
+PREFIX_PATH_STR_OPS="../../SGBDS_Project/StringOperations"
+PREFIX_PATH_BUSINESS_ENTITIES="../../SGBDS_Project/BUSINESS_ENTITIES"
+PREFIX_PATH_HANDLERS="../../SGBDS_Project/HANDLERS"
+PREFIX_PATH_GRAPH_GEN="../../SGBDS_Project/GRAPH_Generators"
+
+LIVRABLE_INDEX=$1
+
+# here we will conidere three livrable then we will have other bash scripts for running the main  
+# Livrable 1
+if [[ "$1" == "livrable1" ]]; then 
+    g++ -g\
+        ./SGBDS_Project.cpp \
+        $PREFIX_PATH_STR_OPS/*.cpp \
+        $PREFIX_PATH_BUSINESS_ENTITIES/*.cpp \
+        $PREFIX_PATH_HANDLERS/*.cpp \
+        $PREFIX_PATH_GRAPH_GEN/*.cpp \
+        -o livrable1
+
+# Livrable 2
+elif [[ "$1" == "livrable2" ]]; then 
+    echo "not cofigured yet"
+# Livrable 3
+elif [[ "$1" == "livrable3" ]]; then
+    echo "not cofigured yet"
+    
+else 
+    echo "the name of variable needs to be livrable1 OR livrable2 OR livrable3"
+fi
---
+++ SGBDS_Project/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+export C_A="0.2"
+export C_V="0.4"
+export FIX_COST="1000.0"
+export MIN_WAIT="45"
+export PATTERN_CLUSTERS="cluster [0-9][0-9]* : (T_[0-9][0-9]*( HLP | WS ))*T_[0-9][0-9]* ;"
+
+
+# $2 and $3 will hold the datafile and destination file and $4 only cluster file 
+
+if [[ $# -ne 4 ]] ; then 
+    echo "Please specify the correct livrable to run"
+    exit 1
+fi
+
+# Livrable 1
+if [[ "$1" == "livrable1" ]]; then 
+    ./livrable1 $2 $3 $4
+
+# Livrable 2
+elif [[ "$1" == "livrable2" ]]; then 
+    ./livrable2 $2 $3 $4
+
+# Livrable 3
+elif [[ "$1" == "livrable3" ]]; then
+    ./livrable3 $2 $3 $4 
+
+else 
+    echo "the name of variable needs to be livrable1 OR livrable2 OR livrable3"
+fi
